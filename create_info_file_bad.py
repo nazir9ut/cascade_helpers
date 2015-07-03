991fb487781c7750ea92f209735b4a4c0447aa7c
naz <nazir8ut@gmail.com>
2015-07-03 10:04:28 +0600

    commit

--- create_info_file_bad.py
+++ create_info_file_bad.py
@@ -1,10 +1,22 @@
 from os import listdir
 from os.path import isfile, join
 from PIL import Image
+from string_helpers import *
+
+
+base = '/home/naz/Desktop/1_lbp'
 
 
 
-mypath = "/home/naz/Desktop/CropNumbers/Bad/"
+
+mypath = base + "/Bad/"
+
+
+
+remove_spaces_in_file_names(mypath)
+
+
+
 onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
 
 for file in onlyfiles:
@@ -13,11 +25,11 @@ for file in onlyfiles:
 
 
 
-with open('/home/naz/Desktop/CropNumbers/Bad.dat', 'w') as f:
+with open(base + '/Bad.dat', 'w') as f:
     for file in onlyfiles:
         img = Image.open(mypath + file)
         f.write('Bad/' +  file + '\n')
 
 
 
-print(onlyfiles)
+# print(onlyfiles)
--- create_info_file_good.py
+++ create_info_file_good.py
@@ -1,10 +1,28 @@
 from os import listdir
 from os.path import isfile, join
 from PIL import Image
+from string_helpers import *
 
 
 
-mypath = "/home/naz/Desktop/two_lbp/Good/"
+base = '/home/naz/Desktop/1_lbp'
+
+
+
+
+
+
+
+mypath = base + '/Good/'
+
+
+
+
+remove_spaces_in_file_names(mypath)
+
+
+
+
 onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
 
 for file in onlyfiles:
@@ -13,7 +31,7 @@ for file in onlyfiles:
 
 
 
-with open('/home/naz/Desktop/two_lbp/Good.dat', 'w') as f:
+with open(base + '/Good.dat', 'w') as f:
     for file in onlyfiles:
         img = Image.open(mypath + file)
         f.write('Good/' +  file + ' 1 0 0 ' + str(img.size[0]) + ' ' + str(img.size[1]) + '\n')
